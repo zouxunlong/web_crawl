3d4172b97a985398d525b858b31a7331f37f30cd
zouxunlong <zoux@i2r.a-star.edu.sg>
2023-06-21 13:51:02 +0700

    yes

--- web_crawl/spiders/en_CNA_Spider.py
+++ web_crawl/spiders/en_CNA_Spider.py
@@ -7,18 +7,17 @@ class en_CNA_Spider(scrapy.Spider):
 
     name = "en_CNA"
     allowed_domains = ["www.channelnewsasia.com"]
-    custom_settings={"DOWNLOAD_DELAY" : 0.5}
+    custom_settings = {"DOWNLOAD_DELAY": 0.5}
 
     start_urls_0 = ["https://www.channelnewsasia.com/api/v1/infinitelisting/94f7cd75-c28b-4c0a-8d21-09c6ba3dd3fc?_format=json&viewMode=infinite_scroll_listing&page=%d" %
-                  n for n in range(1, 1685)]
+                    n for n in range(1, 1685)]
     start_urls_1 = ["https://www.channelnewsasia.com/api/v1/infinitelisting/18e56af5-db43-434c-b76b-8c7a766235ef?_format=json&viewMode=infinite_scroll_listing&page=%d" %
-                  n for n in range(1, 89)]
+                    n for n in range(1, 89)]
     start_urls_2 = ["https://www.channelnewsasia.com/api/v1/infinitelisting/f8f0f8b1-004c-486f-ac72-0c927b7b539d?_format=json&viewMode=infinite_scroll_listing&page=%d" %
-                  n for n in range(1, 385)]
+                    n for n in range(1, 385)]
     start_urls_3 = ["https://www.channelnewsasia.com/api/v1/infinitelisting/1da7e932-70b3-4a2e-891f-88f7dd72c9d6?_format=json&viewMode=infinite_scroll_listing&page=%d" %
-                  n for n in range(1, 956)]
+                    n for n in range(1, 956)]
     start_urls = start_urls_0+start_urls_1+start_urls_2+start_urls_3
-
 
     def __init__(self, start_date, end_date):
         self.start_date = start_date
@@ -30,7 +29,8 @@ class en_CNA_Spider(scrapy.Spider):
 
         for item in data["result"]:
 
-            date_time = datetime.strptime(item["release_date"], "%Y-%m-%dT%H:%M:%S%z").replace(tzinfo=None)
+            date_time = datetime.strptime(
+                item["release_date"], "%Y-%m-%dT%H:%M:%S%z").replace(tzinfo=None)
 
             if date_time < self.start_time:
                 return
@@ -38,26 +38,23 @@ class en_CNA_Spider(scrapy.Spider):
                 date = str(date_time.date())
                 title = item["title"]
 
-                yield scrapy.Request(
-                    url=item["absolute_url"],
-                    callback=self.parse_article,
-                    cb_kwargs={"date": date, "title": title})
-
+                yield scrapy.Request(url=item["absolute_url"],
+                                     callback=self.parse_article,
+                                     cb_kwargs={"date": date, "title": title})
 
     def parse_article(self, response, *args, **kwargs):
         date = kwargs["date"]
         title = kwargs["title"]
         text_nodes = response.xpath('//div[@class="text-long"]/p')
-        texts=[''.join(text_node.xpath(".//text()").getall()).replace(u'\xa0', " ").replace(u'\u3000', " ") for text_node in text_nodes]
+        texts = [''.join(text_node.xpath(".//text()").getall()).replace(u'\xa0',
+                                                                        " ").replace(u'\u3000', " ") for text_node in text_nodes]
         text = "\n".join(texts[:])
         if text:
             yield {"date": date,
                    "title": title,
                    "text": text}
 
-            
     def warn_on_generator_with_return_value_stub(spider, callable):
         pass
     scrapy.utils.misc.warn_on_generator_with_return_value = warn_on_generator_with_return_value_stub
     scrapy.core.scraper.warn_on_generator_with_return_value = warn_on_generator_with_return_value_stub
-
