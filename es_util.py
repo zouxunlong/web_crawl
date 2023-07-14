0b1f544a6a0c04f0b1380266a96734c8641bece4
zouxunlong <zoux@i2r.a-star.edu.sg>
2023-07-14 21:52:09 +0700

    yes

--- es_util.py
+++ es_util.py
@@ -8,6 +8,7 @@ import os
 ES_CONNECTION_STRING = "http://localhost:9200"
 es = Elasticsearch(ES_CONNECTION_STRING).options(ignore_status=400)
 
+
 class Reg_Exp:
     pattern_punctuation = r"""[!?,.:;"#$£€%&'()+-/<≤=≠≥>@[\]^_{|}，。、—‘’“”：；【】￥…《》？！（）]"""
     pattern_url = r"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
@@ -21,6 +22,21 @@ class Reg_Exp:
     pattern_japanese = r"[\u3040-\u30ff\u31f0-\u31ff]"
     pattern_vietnamese = r"[àáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ]"
     pattern_emoji = r'[\U0001F1E0-\U0001F1FF\U0001F300-\U0001F64F\U0001F680-\U0001FAFF\U00002702-\U000027B0]'
+
+
+def unwanted_character_filtered(text_for_filter):
+    text = re.sub(r'[^a-zA-Z0-9\s\t{}{}{}{}{}{}{}{}{}{}]'.format(Reg_Exp.pattern_punctuation[1:-1],
+                                                                 Reg_Exp.pattern_arabic[1:-1],
+                                                                 Reg_Exp.pattern_chinese[1:-1],
+                                                                 Reg_Exp.pattern_tamil[1:-1],
+                                                                 Reg_Exp.pattern_thai[1:-1],
+                                                                 Reg_Exp.pattern_russian[1:-1],
+                                                                 Reg_Exp.pattern_korean[1:-1],
+                                                                 Reg_Exp.pattern_japanese[1:-1],
+                                                                 Reg_Exp.pattern_vietnamese[1:-1],
+                                                                 Reg_Exp.pattern_emoji[1:-1],
+                                                                 ), '', text_for_filter)
+    return text
 
 
 def unwanted_character_detected(text_for_detect):
@@ -41,20 +57,24 @@ def unwanted_character_detected(text_for_detect):
         return True
     return False
 
+
 def tamil_detected(text):
     if re.search(Reg_Exp.pattern_tamil, text):
         return True
     return False
+
 
 def vietnamese_detected(text):
     if re.search(Reg_Exp.pattern_vietnamese, text):
         return True
     return False
 
+
 def chinese_detected(text):
     if re.search(Reg_Exp.pattern_chinese, text):
         return True
     return False
+
 
 def thai_detected(text):
     if re.search(Reg_Exp.pattern_thai, text):
@@ -65,19 +85,20 @@ def thai_detected(text):
 def put_news_article(input_path="/home/xuancong/web_crawl/data"):
     for rootdir, dirs, files in os.walk(input_path):
         source = rootdir.split("/")[-1]
-        language_type=source[:2]
+        language_type = source[:2]
         files.sort(reverse=True)
-        if language_type in ['en','zh','vi','th','ta','ms','id']:
-            index='news_articles_'+language_type
+        if language_type in ['en', 'zh', 'vi', 'th', 'ta', 'ms', 'id']:
+            index = 'news_articles_'+language_type
             for file in files:
                 if file.endswith('.jsonl'):
                     input_file = os.path.join(rootdir, file)
                     with open(input_file, encoding='utf8') as file_in:
                         for line in file_in:
-                            item=json.loads(line)
+                            item = json.loads(line)
 
                             if unwanted_character_detected(item['text']):
-                                continue
+                                item['text'] = unwanted_character_filtered(item['text'])
+
                             if 'ta' in [language_type] and not tamil_detected(item['text']):
                                 continue
                             if 'vi' in [language_type] and not vietnamese_detected(item['text']):
@@ -87,19 +108,20 @@ def put_news_article(input_path="/home/xuancong/web_crawl/data"):
                             if not item['text'].strip():
                                 continue
 
-                            doc={
+                            doc = {
                                 '_index': index,
                                 '_id': Simhash(item['text'], f=64, reg=r'[\S]').value,
                                 'language_type': language_type,
                                 'source': source,
                                 'title': item['title'],
                                 'text': item['text'],
-                                'date': item['date'],                            
-                                }
+                                'date': item['date'],
+                            }
 
                             yield doc
-                            
-response=bulk(client=es, actions=put_news_article())
+
+
+response = bulk(client=es, actions=put_news_article())
 print(response, flush=True)
 
 
