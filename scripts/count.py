6985150920178e1baefe5bf76a3230626932691e
zxl <449326540@qq.com>
2023-11-06 10:09:53 +0800

    yes

---
+++ scripts/count.py
@@ -0,0 +1,5 @@
+
+
+
+documents=open("/home/xuanlong/web_crawl/documentIds_remain.txt").readlines()
+print(len(documents))
--- scripts/test0.py
+++ scripts/test0.py
@@ -2,10 +2,12 @@
 
 
 documentIds=open("/home/xuanlong/web_crawl/documentIds.txt").readlines()
-used_documentIds=open("/home/xuanlong/web_crawl/used_documentIds.txt").readlines()
-
+used_documentIds0=open("/home/xuanlong/web_crawl/used_documentIds.txt").readlines()
+used_documentIds1=open("/home/xuanlong/web_crawl/used_documentIds1.txt").readlines()
+used_documentIds=used_documentIds0+used_documentIds1
 set1 = set(documentIds)
 set2 = set(used_documentIds)
+
 res = list(set1 - set2)
 
 open(file="/home/xuanlong/web_crawl/documentIds_remain.txt",mode="w",encoding="utf8").write("".join(res))
